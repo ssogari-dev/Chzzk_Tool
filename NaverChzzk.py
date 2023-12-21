3d81fb74edf47cee7e6d0f9ad9929807aabdd0bb
SSoGari Dev. Studio <hwang0911_02@naver.com>
2023-12-21 11:38:21 +0900

    Update for Fetching VOD Info

--- NaverChzzk.py
+++ NaverChzzk.py
@@ -1,15 +1,26 @@
 import re, json, requests
-# import m3u8
 from streamlink.plugin import Plugin, pluginmatcher
-from streamlink.stream import HLSStream
+from streamlink.stream import HLSStream, DASHStream
 
-@pluginmatcher(re.compile(r'https://chzzk\.naver\.com/live/(?P<channel_id>\w+)'))
+@pluginmatcher(re.compile(
+    r'https?://chzzk\.naver\.com/(?:video/(?P<video_no>\d+)|live/(?P<channel_id>[^/?]+))$',
+))
 class ChzzkPlugin(Plugin):
-    API_URL = "https://api.chzzk.naver.com/service/v1/channels/{channel_id}/live-detail"
+    LIVE_INFO = "https://api.chzzk.naver.com/service/v1/channels/{channel_id}/live-detail"
+    VOD_URL = "https://apis.naver.com/neonplayer/vodplay/v1/playback/{video_id}?key={in_key}"
+    VOD_INFO = "https://api.chzzk.naver.com/service/v1/videos/{video_no}"
 
     def _get_streams(self):
         channel_id = self.match.group("channel_id")
-        api_url = self.API_URL.format(channel_id=channel_id)
+        video_no = self.match.group("video_no")
+
+        if channel_id:
+            return self._get_live_streams(channel_id)
+        elif video_no:
+            return self._get_vod_streams(video_no)
+        
+    def _get_live_streams(self, channel_id):
+        api_url = self.LIVE_INFO.format(channel_id=channel_id)
 
         try:
             response = requests.get(api_url)
@@ -29,24 +40,51 @@ class ChzzkPlugin(Plugin):
                 self.logger.error("Channel is not live (status: {0})".format(status))
                 return
 
-            live_title = content.get('liveTitle')
-            channel_name = content.get('channel', {}).get('channelName')
-            category = content.get('liveCategory')
+            self.author = content.get('channel', {}).get('channelName')
+            self.category = content.get('liveCategory')
+            self.title = content.get('liveTitle')
             stream_info = content.get('livePlaybackJson')
-            hls_url = json.loads(stream_info).get('media', [{}])[0].get('path')
-            # hls = m3u8.loads(hls_url)
             
-            # self.logger.info("Stream Title: {0}".format(live_title))
-            # self.logger.info("Channel Name: {0}".format(channel_name))
-            # self.logger.info("Category: {0}".format(category))
-            # self.logger.info("HLS URL: {0}".format(hls_url))
-            self.author = channel_name
-            self.category = category
-            self.title = live_title
+            hls_url = json.loads(stream_info).get('media', [{}])[0].get('path')
 
             yield from HLSStream.parse_variant_playlist(self.session, hls_url).items()
+
         except json.JSONDecodeError as e:
             self.logger.error("Failed to decode JSON response: {0}".format(str(e)))
             return
         
+    def _get_vod_streams(self, video_no):
+        api_url = self.VOD_INFO.format(video_no=video_no)
+
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            self.logger.error("Failed to fetch video information: {0}".format(str(e)))
+            return
+
+        if response.status_code == 404:
+            self.logger.error("Video not found")
+            return
+        
+        try:
+            content = response.json().get('content', {})
+            video_id = content.get('videoId')
+            in_key = content.get('inKey')
+            video_url = self.VOD_URL.format(video_id=video_id, in_key=in_key)
+
+            self.author = content.get('channel', {}).get('channelName')
+            self.category = content.get('videoCategory')
+            self.title = content.get('videoTitle')
+            self.vod_date = content.get('liveOpenDate')[0:10]
+            
+            yield from DASHStream.parse_manifest(
+                self.session, video_url,
+                headers={"Accept": "application/dash+xml"}
+            ).items()
+
+        except json.JSONDecodeError as e:
+            self.logger.error("Failed to decode JSON response: {0}".format(str(e)))
+            return
+
 __plugin__ = ChzzkPlugin
